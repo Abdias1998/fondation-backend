18fd9d281d44b687c1c5d6e87851de697004198e
Abdias <lagraceparle98@gmail.com>
2023-08-16 14:12:04 +0100

    cors ajout

--- index.js
+++ index.js
@@ -13,15 +13,26 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT;
 const origineClient = process.env.CLIENT_URL;
-app.use(cors({ credentials: true, origin: origineClient })); //L'origine des requêtes
-// app.use(useragent.express());
-// app.use(cookieParser()); //Lire les cookies
+// app.use(cors({ credentials: true, origin: origineClient })); //L'origine des requêtes
+app.use(
+  cors({
+    origin: "https://capable-creponne-29ce5a.netlify.app",
+    credentials: true,
+  })
+);
+
+app.use(useragent.express());
+app.use(cookieParser()); //Lire les cookies
 app.use(cookieParser());
 app.use(helmet());
 
 app.use(bodyParser.json()); //Transformer nos corps en json
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "./client/build")));
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build", "index.html"));
+});
 app.use("/api/v1", subscribeRoute);
 app.listen(port, () =>
   console.log(`Le serveur est démarrer sur le port ${port}`)
