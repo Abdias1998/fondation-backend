03709b8c41b9b78c67e4ff403e494d646137e367
Abdias <lagraceparle98@gmail.com>
2023-08-16 13:57:44 +0100

    suppression et ajout de credatial

--- index.js
+++ index.js
@@ -13,12 +13,12 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT;
 const origineClient = process.env.CLIENT_URL;
-app.use(useragent.express());
+app.use(cors({ credentials: true, origin: origineClient })); //L'origine des requêtes
+// app.use(useragent.express());
 // app.use(cookieParser()); //Lire les cookies
 app.use(cookieParser());
 app.use(helmet());
 
-app.use(cors({ origin: origineClient })); //L'origine des requêtes
 app.use(bodyParser.json()); //Transformer nos corps en json
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "./client/build")));
