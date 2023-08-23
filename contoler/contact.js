42a949a840a8a458dfe0fd6fe90a37e9ff3c269e
Abdias <lagraceparle98@gmail.com>
2023-08-23 09:17:40 +0100

    Commit automatisé

--- contoler/contact.js
+++ contoler/contact.js
@@ -1,9 +1,13 @@
 // /* global __dirname */
+/* global process*/
 
-const SUBSCRIBE = require("../model/contact");
+// const SUBSCRIBE = require("../model/contact");
 const express_async = require("express-async-handler");
 const validator = require(`validator`);
+const sendEmail_request = require(`../utils/send.email`);
+const fs = require("fs");
 
+const sendEmail = sendEmail_request.sendEmail;
 module.exports.receiveMessage = express_async(async (req, res) => {
   const faq = [
     "Foi et Croyances",
@@ -14,12 +18,11 @@ module.exports.receiveMessage = express_async(async (req, res) => {
     "Engagement Social et Humanitaire",
     "Questions Théologiques",
     "Contact et Support",
-    "Partenariats et Collaborations",
     "Ressources et Enseignements",
   ];
 
   const { names, subject, email, message } = req.body;
-  let existingUser;
+  // let existingUser;
   // Vérification si c'est un email valide et non vide
   if (!validator.isEmail(email) || validator.isEmpty(email)) {
     return res
@@ -31,34 +34,59 @@ module.exports.receiveMessage = express_async(async (req, res) => {
       message: `La longueur de votre message ne respecte pas nos standards de validation.`,
     });
   // Recherche de l'utilisateur dans la base de données
-  existingUser = await SUBSCRIBE.findOne({ email });
+  // existingUser = await SUBSCRIBE.findOne({ email });
 
-  if (existingUser) {
-    return res
-      .status(400)
-      .json({
-        message:
-          "Nous avons déjà répondu à l'une de vos préoccupations. Veuillez nous contacter directement par e-mail ou WhatsApp pour toute communication ultérieure",
-      });
-  }
+  // if (existingUser) {
+  //   return res.status(400).json({
+  //     message:
+  //       "Nous avons déjà répondu à l'une de vos préoccupations. Veuillez nous contacter directement par e-mail ou WhatsApp pour toute communication ultérieure",
+  //   });
+  // }
 
   if (faq.includes(subject.trim())) {
-    return res
-      .status(400)
-      .json({
-        message:
-          "Nous disposons d'une liste de questions sur ce sujet. Nous vous encourageons à consulter la section FAQ pour obtenir de plus amples informations.",
-      });
+    return res.status(400).json({
+      message:
+        "Nous disposons d'une liste de questions sur ce sujet. Nous vous encourageons à consulter la section FAQ dans la rubrique contactez-nous pour obtenir de plus amples informations.",
+    });
   }
 
   try {
-    existingUser = new SUBSCRIBE({
-      email,
-      subject,
-      message,
-      names,
+    // existingUser = new SUBSCRIBE({
+    //   email,
+    //   subject,
+    //   message,
+    //   names,
+    // });
+    fs.readFile("./template/contact.html", "utf-8", async (err, data) => {
+      if (err) {
+        return res
+          .status(500)
+          .json({ message: "Erreur lors de la lecture du modèle" });
+      } else {
+        try {
+          // Envoi de l'email avec le contenu du template
+          const html = data
+            .replace(/{name}/g, names)
+            .replace(/{subject}/g, subject)
+            .replace(/{email}/g, email)
+            .replace(/{message}/g, message);
+
+          await sendEmail(
+            `${process.env.USER}`,
+            "Fondation La Grâce Parle - Août 2023",
+            html
+          );
+          res.status(200).json({
+            message: `Nous avons reçu votre message, nous vous reviendrons au plus vite!`,
+          });
+        } catch (error) {
+          return res.status(500).json({
+            message: "L'envoi de l'email a échoué, veuillez réessayer",
+          });
+        }
+      }
     });
-    await existingUser.save();
+    // await existingUser.save();
     return res.status(200).json({
       message: `Nous avons reçu votre message ${names}, nous vous reviendrons au plus vite!`,
     });
